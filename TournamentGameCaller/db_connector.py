20fa0b7ca334d480c27777d2bdb36ab774e13d2a
Sven <sven.hoyer@hotmail.com>
2022-01-24 13:38:17 +0100

    remove if for discipline and black style changes

--- TournamentGameCaller/db_connector.py
+++ TournamentGameCaller/db_connector.py
@@ -48,30 +48,29 @@ class DB_Connector:
                 + ""
             )
             player2 = cursor.fetchall()
-            if row[0][:2] == "JE":
-                game = (
-                    self.getDisciplineName(row[0][:2])
-                    + row[0][2:]
-                    + " auf Spielfeld "
-                    + str(int(row[1]))
-                    + ": "
-                    + player1[0][1]
-                    + " "
-                    + player1[0][0]
-                    + " gegen "
-                    + player2[0][1]
-                    + " "
-                    + player2[0][0]
-                )
-                print(game)
-                speech = gTTS(text=game, lang=language, slow=False)
-                speech.save("audio/test.mp3")
+            game = (
+                self.getDisciplineName(row[0][:2])
+                + row[0][2:]
+                + " auf Spielfeld "
+                + str(int(row[1]))
+                + ": "
+                + player1[0][1]
+                + " "
+                + player1[0][0]
+                + " gegen "
+                + player2[0][1]
+                + " "
+                + player2[0][0]
+            )
+            print(game)
+            speech = gTTS(text=game, lang=language, slow=False)
+            speech.save("audio/test.mp3")
 
-                player = vlc.MediaPlayer(dir_path + "/audio/test.mp3")
-                player.play()
-                time.sleep(10)
+            player = vlc.MediaPlayer(dir_path + "/audio/test.mp3")
+            player.play()
+            time.sleep(10)
 
-    def getDisciplineName(self, disciplineShortcut):
+    def getDisciplineName(self, disciplineShortcut:str) -> str:
         disciplineNames = {
             "JE": "Jungeneinzel",
             "ME": "Mädcheneinzel",

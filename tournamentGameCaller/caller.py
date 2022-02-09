5028ea0b35cbfb24d1eb0732a6ccd345fa039745
Sven <sven.hoyer@hotmail.com>
2022-02-09 20:26:09 +0100

    add Docstrings to all Files

--- tournamentGameCaller/caller.py
+++ tournamentGameCaller/caller.py
@@ -1,12 +1,24 @@
+# Standard Library
 import time
 
+# Third Party
 import vlc
+
 from gtts import gTTS
+
 
 LANGUAGE = "de"
 
 
 def single_caller(discipline, age_class, court, players) -> None:
+    """Bring all parts of the single call together and call text_to_speech function
+
+    Args:
+        discipline (str): [description]
+        age_class (str): [description]
+        court (str): [description]
+        players (list): [description]
+    """
     game = (
         discipline
         + age_class
@@ -27,6 +39,14 @@ def single_caller(discipline, age_class, court, players) -> None:
 
 
 def double_caller(discipline, age_class, court, players) -> None:
+    """Bring all parts of the double call together and call text_to_speech function
+
+    Args:
+        discipline (str): [description]
+        age_class (str): [description]
+        court (str): [description]
+        players (list): [description]
+    """
     game = (
         discipline
         + age_class
@@ -53,13 +73,23 @@ def double_caller(discipline, age_class, court, players) -> None:
     text_to_speech(game)
 
 
-def text_to_speech(game_text) -> None:
+def text_to_speech(game_text: str) -> None:
+    """Take Text and do magic
+
+    Args:
+        game_text (str): callable text
+    """
     speech = gTTS(text=game_text, lang=LANGUAGE, slow=False)
     speech.save("audio/game.mp3")
     caller("audio/game.mp3")
 
 
-def caller(audio) -> None:
+def caller(audio: str) -> None:
+    """Call the audio from text to speech engine via vlc Player
+
+    Args:
+        audio (str): relative path to the audio file
+    """
     instance = vlc.Instance()
     player = instance.media_player_new()
     media = instance.media_new(audio)
--- tournamentGameCaller/config.py
+++ tournamentGameCaller/config.py
@@ -1,11 +1,19 @@
+# Standard Library
 from configparser import SafeConfigParser
 
 
 class Configuration:
+    """Manage and get Configurations from config/config.cfg for the Package"""
+
     config = SafeConfigParser()
     config.read("config/config.cfg")
 
     def get_database_config(self) -> dict:
+        """Get Database Configurations
+
+        Returns:
+            dict: Database Configurations Driver, Path and Password
+        """
         database_config = {}
         database_config["Driver"] = self.config["DATABASE"]["Driver"]
         database_config["Path"] = self.config["DATABASE"]["Path"]
@@ -13,4 +21,12 @@ class Configuration:
         return database_config
 
     def get_discipline_name(self, discipline_shortcut: str) -> str:
+        """Get the full discipline name by shortcut
+
+        Args:
+            discipline_shortcut (str): Input the shortcut for a discipline
+
+        Returns:
+            str: full discipline name
+        """
         return self.config["DISCIPLINES"][discipline_shortcut]
